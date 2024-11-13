f74ca8afcabff583580e5bc13695914455ebe165
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-11-13 16:31:10 +0530

    added graphql code

--- src/test/java/com/qa/api/tests/GraphQLMutationUser.java
+++ src/test/java/com/qa/api/tests/GraphQLMutationUser.java
@@ -1,13 +1,22 @@
 package com.qa.api.tests;
 
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class GraphQLMutationUser {
     
@@ -100,4 +109,81 @@ public class GraphQLMutationUser {
             .body("data.createUser.user.status", equalTo("active"))
             .body("data.createUser.user.id", notNullValue());
     }
+    
+    
+    
+    
+    private String readGraphQLQuery(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    @Test
+    public void testCreateUserMutationUsingMapAndGraphqlFile() throws IOException {
+    	
+    	//1. mutate(create) a user using mutation:
+        String newEmailId = getRandomEmailID();
+        String mutationTemplate = readGraphQLQuery("src/test/resources/graphql/createUserMutation.graphql");
+        String mutation = String.format(mutationTemplate, newEmailId);
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("query", mutation);
+
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer e4b8e1f593dc4a731a153c5ec8cc9b8bbb583ae964ce650a741113091b4e2ac6")
+            .body(requestBody)
+        .when()
+            .post(GRAPHQL_ENDPOINT);
+        
+        //fetch the user id:
+        Integer userId = response.jsonPath().getInt("data.createUser.user.id");
+        System.out.println("user id --->" + userId);
+        
+        response.then().log().all()
+        .assertThat()   
+        .statusCode(200)
+            .body("data.createUser.user.name", equalTo("naveen"))
+            .body("data.createUser.user.email", equalTo(newEmailId))
+            .body("data.createUser.user.gender", equalTo("male"))
+            .body("data.createUser.user.status", equalTo("active"))
+            .body("data.createUser.user.id", notNullValue());
+        
+        System.out.println("=============================");
+        
+        //2. create the graphql query with the userId and validate the user.
+        String query = readGraphQLQuery("src/test/resources/graphql/getUserQuery.graphql");
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("userId", userId);
+
+        Map<String, Object> requestBodyGet = new HashMap<>();
+        requestBodyGet.put("query", query);
+        requestBodyGet.put("variables", variables);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer e4b8e1f593dc4a731a153c5ec8cc9b8bbb583ae964ce650a741113091b4e2ac6")
+            .body(requestBodyGet)
+        .when()
+            .post(GRAPHQL_ENDPOINT)
+        .then().log().all()
+            .statusCode(200)
+            .body("data.user.id", equalTo(userId))
+            .body("data.user.email", equalTo(newEmailId))
+            .body("data.user.name", not(emptyString()))
+            .body("data.user.gender", not(emptyString()))
+            .body("data.user.status", not(emptyString()));
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- src/test/java/com/qa/api/tests/GraphQLQueryUser.java
+++ src/test/java/com/qa/api/tests/GraphQLQueryUser.java
@@ -1,11 +1,19 @@
 package com.qa.api.tests;
 
-import io.restassured.http.ContentType;
-import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
 
 public class GraphQLQueryUser {
 	
@@ -26,6 +34,37 @@ public class GraphQLQueryUser {
 
         Map<String, Object> variables = new HashMap<>();
         variables.put("userId", "7516106");
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("query", query);
+        requestBody.put("variables", variables);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer e4b8e1f593dc4a731a153c5ec8cc9b8bbb583ae964ce650a741113091b4e2ac6")
+            .body(requestBody)
+        .when()
+            .post(GRAPHQL_ENDPOINT)
+        .then().log().all()
+            .statusCode(200)
+            .body("data.user.id", equalTo(7516106))
+            .body("data.user.email", not(emptyString()))
+            .body("data.user.name", not(emptyString()))
+            .body("data.user.gender", not(emptyString()))
+            .body("data.user.status", not(emptyString()));
+    }
+    
+    
+    private String readGraphQLQuery(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    @Test
+    public void testGetUserQueryWithVariablesWithGraphqlFile() throws IOException {
+        String query = readGraphQLQuery("src/test/resources/graphql/getUserQuery.graphql");
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("userId", 7516106);
 
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("query", query);
